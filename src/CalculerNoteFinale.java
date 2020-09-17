bcf10abf9a8de95f21c5247cc376c21a702fe629
dev88jerry <70291890+dev88jerry@users.noreply.github.com>
2020-09-17 09:34:36 -0400

    Update CalculerNoteFinale.java

--- src/CalculerNoteFinale.java
+++ src/CalculerNoteFinale.java
@@ -21,26 +21,90 @@ public class CalculerNoteFinale {
     public static void main(String[] args) {
         // TODO code application logic here       
         ArrayList<String> inp = new ArrayList<String>();
+        ArrayList<Etudiant> groupe = new ArrayList<Etudiant>();
+        String str[] = null;
+
+        double moy = 0.0;
+        int failE = 0;
+        int eOverAvg = 0;
+
+        String oFile = "C:/Users/Admin/Documents/NetBeansProjects/CalculerNoteFinale/notes.txt";
 
         try {
-            Scanner sc = new Scanner(new File("C:/Users/Admin/Documents/NetBeansProjects/CalculerNoteFinale/notes.txt"));
+            Scanner sc = new Scanner(new File(oFile));
 
             while (sc.hasNextLine()) {
+                Etudiant e = new Etudiant();
                 inp.add(sc.nextLine());
+                for (String st : inp) {
+                    str = st.split(" ");
+                }
+                for (String st : str) {
+                    if (st.matches("[a-zA-Z]+")) {
+                        e.setNom(st);
+                    } else {
+                        double d = Double.parseDouble(st);
+                        e.ajouteNote(d);
+                    }
+                }
+                e.passEtu();
+                groupe.add(e);
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(CalculerNoteFinale.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        inp.add("New line");
-        
-        String newS = "Next line";
-        inp.add(newS);
-        
-        for (String st : inp) {
-            System.out.println(st);
+        /*
+                Collections.sort(groupe, new Comparator(){
+            @Override
+            public int compare(Object e1, Object e2){
+                return ((Etudiant)e1).getNom().compareTo(((Etudiant)e2).getNom());                
+            }        
+        });
+         */
+        //sort alphabetical
+        Collections.sort(groupe, (Object e1, Object e2) -> ((Etudiant) e1).getNom().compareTo(((Etudiant) e2).getNom()));
+
+        /**
+         * print all students & grades w/pass-fail
+         */
+        for (Etudiant e : groupe) {
+            System.out.print("Nom: " + e.getNom());
+            System.out.print("\tNote: " + e.getNotes());
+            if (e.isFail()) {
+                System.out.println(" EC");
+                failE++;
+            } else {
+                System.out.println("");
+            }
+            moy += e.getNotes();
         }
 
+        System.out.println("");
+        System.out.println("Nombre total d'etudiants: " + groupe.size());
+        System.out.println("Nombre d'echecs: " + failE);
+        moy = moy / groupe.size();
+        System.out.println("Moyenne du groupe: " + moy);
+        
+        double tGrade = 0.0;
+        double bGrade = 0.0;
+        int i=0;
+        int loc = 0;
+        for (Etudiant e : groupe){
+            if(e.getNotes() > moy){
+                eOverAvg++;
+            }
+            
+            tGrade = e.getNotes();
+            if(tGrade > bGrade){
+                bGrade = tGrade;
+                loc = i;
+            }
+            i++;            
+        }        
+        
+        System.out.println("Meilleur(e) etudiant(e): " + groupe.get(loc).getNom());
+        System.out.println("Nb. d'etudiant supp. a la moyenne: " + eOverAvg);
     }
-
+    
 }
